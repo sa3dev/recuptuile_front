5223fc96d492aa7d508ee9aee440f5773ccc8421
sa3dev <sa33dev@gmail.com>
2020-02-17 16:41:48 +0100

    update some style for responsive in card recup

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactComponent } from './contact.component';
 import { FormsCustomModule } from '../component/forms/forms.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -9,6 +10,8 @@ import { FormsCustomModule } from '../component/forms/forms.module';
   imports: [
     CommonModule,
     FormsCustomModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [ContactComponent]
 })
